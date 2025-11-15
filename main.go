abfc7557d9276e76d5b409911be54102f0a5affb
aaronb12-ux <afairster03@gmail.com>
2025-11-14 19:10:20 -0800

    made 'createMapHandler' func

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
-
 	"net/http"
 	"fmt"
+
 )
 
 func createMapHandler(pathToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+     
+	return func (w http.ResponseWriter, r *http.Request) {
+		
+		path, exists := pathToUrls[r.URL.Path] //check if the current url path exists in the map 
 
-	return nil
+		if exists { //if it does then redirect user
+			http.Redirect(w, r, path, http.StatusSeeOther)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}
 }
+
+func createYAMlHandler(YAML []byte, fallback http.Handler) (http.HandlerFunc, error) {
+
+	//parse YAML
+
+	//convert to map
+}
+
+
+
 
 
 func main() {
@@ -20,10 +39,23 @@ func main() {
 		"/dogbreed" : "https://www.petfinder.com/dogs-and-puppies/breeds/",
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 	}
-    
-	
-	
 
+	mapHandler := createMapHandler(pathsToUrls, mux)
+
+	yaml := `
+			- path: /urlshort
+  				url: https://github.com/gophercises/urlshort
+			- path: /urlshort-final
+  				url: https://github.com/gophercises/urlshort/tree/solution
+			`
+	yamlHandler, err := createYAMlHandler([]byte(yaml), mapHandler)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", mapHandler)
 
 }
 

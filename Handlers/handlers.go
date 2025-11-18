67537e7678f9a00fa2ddf45793848f74d42e2083
aaronb12-ux <afairster03@gmail.com>
2025-11-17 19:19:16 -0800

    fixed YAML mapping and added 'Handlers' package.

---
+++ Handlers/handlers.go
@@ -0,0 +1,70 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+	"gopkg.in/yaml.v3"
+)
+
+    
+type PathURL struct { //struct we unmarshal the YAML into
+    Path string `yaml:"path"`
+    URL  string `yaml:"url"`
+}
+
+func ParseYAML(YAML []byte) []PathURL { //unmarshal the YAML into the 'PathURL' struct. This creates a slice of 'PathURL' objects
+
+	var parsed []PathURL
+
+	if err := yaml.Unmarshal(YAML, &parsed); err != nil { 
+		log.Fatal(err)
+	}
+
+	return parsed
+}
+
+
+func YamlToMap(yaml []PathURL) map[string]string { //convert the parsed yaml into a map for easier key value access
+
+	m := make(map[string]string)
+
+	for _, element := range yaml {
+		m[element.Path] = element.URL
+	}
+
+	return m
+}
+
+
+func CreateMapHandler(pathToUrls map[string]string, fallback http.Handler) http.HandlerFunc { 
+     
+	return func (w http.ResponseWriter, r *http.Request) {
+		
+		path, exists := pathToUrls[r.URL.Path] //check if the current url path exists in the map 
+
+		if exists { //if it does then redirect user
+			http.Redirect(w, r, path, http.StatusSeeOther)
+		} else {
+			fallback.ServeHTTP(w, r) 
+		}
+	}
+}
+
+
+func CreateYAMlHandler(YAML []byte, fallback http.Handler) (http.HandlerFunc) {
+	 
+	parsedYAML := ParseYAML(YAML)
+	
+	yamlToMap := YamlToMap(parsedYAML)
+
+	return func (w http.ResponseWriter, r *http.Request) {
+		
+		path, exists := yamlToMap[r.URL.Path]
+
+		if exists {
+			http.Redirect(w, r, path, http.StatusSeeOther)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -2,71 +2,10 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"gopkg.in/yaml.v3"
+	"aaronb.com/urlshortener/handlers"
+
 )
-
-    
-type PathURL struct {
-    Path string `yaml:"path"`
-    URL  string `yaml:"url"`
-}
-
-func parseYAML(YAML []byte) []PathURL { //unmarshal the YAML into the 'PathURL' struct. This creates a slice of 'PathURL' objects
-
-	var parsed []PathURL
-
-	if err := yaml.Unmarshal(YAML, &parsed); err != nil { 
-		log.Fatal(err)
-	}
-
-	return parsed
-
-}
-
-func yamlToMap(yaml []PathURL) map[string]string { //convert the parsed yaml into a map for easier key value access
-
-	m := make(map[string]string)
-
-	for _, element := range yaml {
-		m[element.URL] = element.Path	
-	}
-
-	return m
-}
-
-func createMapHandler(pathToUrls map[string]string, fallback http.Handler) http.HandlerFunc { 
-     
-	return func (w http.ResponseWriter, r *http.Request) {
-		
-		path, exists := pathToUrls[r.URL.Path] //check if the current url path exists in the map 
-
-		if exists { //if it does then redirect user
-			http.Redirect(w, r, path, http.StatusSeeOther)
-		} else {
-			fallback.ServeHTTP(w, r)
-		}
-	}
-}
-
-
-func createYAMlHandler(YAML []byte, fallback http.Handler) (http.HandlerFunc) {
-	 
-	parsedYAML := parseYAML(YAML)
-	
-	yamlToMap := yamlToMap(parsedYAML)
-
-	return func (w http.ResponseWriter, r *http.Request) {
-		path, exists := yamlToMap[r.URL.Path]
-
-		if exists {
-			http.Redirect(w, r, path, http.StatusSeeOther)
-		} else {
-			fallback.ServeHTTP(w, r)
-		}
-	}
-}
 
 func main() {
 
@@ -75,19 +14,21 @@ func main() {
 	pathsToUrls := map[string]string { //set of mapped shortened urls to actual urls
 		"/dogbreed" : "https://www.petfinder.com/dogs-and-puppies/breeds/",
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+		"/go":     "https://gophercises.com/",
+		
 	}
 
-	mapHandler := createMapHandler(pathsToUrls, mux)
+	mapHandler := handlers.CreateMapHandler(pathsToUrls, mux)
 
 	yaml := `
 - path: /urlshort
   url: https://github.com/gophercises/urlshort
 - path: /urlshort-final
   url: https://github.com/gophercises/urlshort/tree/solution
+- path: /goooooooo
+  url: https://gophercises.com/
 `
-	//yamlHandler, err := createYAMlHandler([]byte(yaml), mapHandler)
-
-	yamlHandler := createYAMlHandler([]byte(yaml), mapHandler)
+	yamlHandler := handlers.CreateYAMlHandler([]byte(yaml), mapHandler)
 	
 	fmt.Println("Starting the server on :8080")
 	http.ListenAndServe(":8080", yamlHandler)
